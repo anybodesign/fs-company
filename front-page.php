195191c3470591ccf3768681a0c5824de04d7954
thomas <atelier@anybodesign.com>
2018-01-23 12:53:07 +0100

    CPT See All Button

--- front-page.php
+++ front-page.php
@@ -88,7 +88,7 @@ get_header(); ?>
 						// Custom Post type Loop 
 						
 						$args_cpt = array(
-							'posts_per_page' 	=> -1,
+							'posts_per_page' 	=> 6,
 							'post_type' 		=> 'service',
 							'order'				=> 'DESC'
 						);
@@ -111,9 +111,23 @@ get_header(); ?>
 							<?php while ($query_cpt->have_posts()) : $query_cpt->the_post(); ?>
 							<div class="col-4 mid-6">
 								<?php get_template_part( 'template-parts/content','cpt' ); ?>
-							</div>					
+							</div>
 							<?php endwhile; ?>
-	
+
+							<?php if(get_theme_mod('select_cpt_page')) { ?>
+							<div class="col-12 txtc">
+								<?php $ctp_link = get_theme_mod('select_cpt_page'); ?>
+								
+								<a href="<?php echo get_permalink($ctp_link); ?>" class="action-btn">
+									<?php if(get_theme_mod('cpt_btn_text')) {
+										echo get_theme_mod('cpt_btn_text', ''); 
+									} else {
+										echo _e('See All','fs-company'); 	
+									} ?>
+								</a>
+							</div>					
+							<?php } ?>
+							
 						</div>
 						
 					</div>					
