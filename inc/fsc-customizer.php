682bf45ab1a36bf3592b6e70a1a0936bbf8a9501
thomas <atelier@anybodesign.com>
2018-01-23 12:52:49 +0100

    Customizer CPT Options

--- inc/fsc-customizer.php
+++ inc/fsc-customizer.php
@@ -35,7 +35,7 @@ function fs_company_customize_register($wp_customize) {
 	)));
 	
 
-	// Display Services
+	// Display CPT
 	
 	$wp_customize->add_setting('display_cpt', array(
 		'default'	=> false,
@@ -44,20 +44,59 @@ function fs_company_customize_register($wp_customize) {
 	
 	$wp_customize->add_control('display_cpt_ctrl', array(
 		'type'			=> 'checkbox',
-		'label'			=> __('Display the services on the front page', 'fs-company'),
+		'label'			=> __('Display your custom posts on the front page', 'fs-company'),
 		'section'		=> 'static_front_page',
 		'settings'		=> 'display_cpt',
 	));
 	
+	
+	// CPT page
+	
+	$wp_customize->add_setting( 'select_cpt_page', array(
+	  'capability' 			=> 'edit_theme_options',
+	  'sanitize_callback' 	=> 'fs_sanitize_dropdown_pages',
+	));
+	
+	$wp_customize->add_control( 'select_cpt_page_ctrl', array(
+	  'type' 			=> 'dropdown-pages',
+	  'section' 		=> 'static_front_page', 
+	  'label'			=> __( 'Custom Posts Page', 'fs-company' ),
+	  'description' 	=> __( 'Select the page where your custom posts are.', 'fs-company' ),
+	  'settings'		=> 'select_cpt_page',
+	));
+
+	// CPT button text
+	
+	$wp_customize->add_setting('cpt_btn_text', array(
+		'default'				=> '',
+		'sanitize_callback'		=> 'sanitize_text_field'
+	));
+	$wp_customize->add_control('cpt_btn_text_ctrl', array(
+		'label'			=> __('Custom posts button text', 'fs-company'),
+		'description'	=> __('Add a custom text for the "See All" button.', 'fs-company'),
+		'section'		=> 'static_front_page',
+		'settings'		=> 'cpt_btn_text',
+	));	
 	
 	 
 }
 add_action('customize_register', 'fs_company_customize_register');
 
 
-// Sanitize
+// Sanitize /////////
+
+// Dropdown
+
+function fs_sanitize_dropdown_pages( $page_id, $setting ) {
+  // Ensure $input is an absolute integer.
+  $page_id = absint( $page_id );
+
+  // If $page_id is an ID of a published page, return it; otherwise, return the default.
+  return ( 'publish' == get_post_status( $page_id ) ? $page_id : $setting->default );
+}	
 
 // Checkbox
+
 function fsc_customizer_sanitize_checkbox( $input ) {
 	if ( $input === true || $input === '1' ) {
 		return '1';
