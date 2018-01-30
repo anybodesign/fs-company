66e123b33fee3bd3e515bbfe113a10a7b28f92ba
thomas <atelier@anybodesign.com>
2018-01-30 11:10:45 +0100

    a11y logo

--- template-parts/header-logo.php
+++ template-parts/header-logo.php
@@ -13,10 +13,11 @@
 				
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php _e('Go to Home Page', 'fs-blog'); ?>">
 						<?php if ( is_front_page() && get_theme_mod('site_logo_white') ) { // white version  ?>
-						<img class="logo-white" src="<?php echo(get_theme_mod('site_logo_white', 'none')); ?>" alt="<?php echo esc_url(bloginfo('name')); ?> logo">
+						<img class="logo-white" src="<?php echo(get_theme_mod('site_logo_white', 'none')); ?>" alt="<?php echo esc_url(bloginfo('name')); ?>">
 						<?php } ?>
 						<?php if ( get_theme_mod('site_logo') ) { ?>
-						<img class="logo" src="<?php echo(get_theme_mod('site_logo', 'none')); ?>" alt="<?php echo esc_url(bloginfo('name')); ?> logo">
-						<?php } ?>
-						<span><?php bloginfo( 'name' ); ?></span>
+						<img class="logo" src="<?php echo(get_theme_mod('site_logo', 'none')); ?>" alt="<?php echo esc_url(bloginfo('name')); ?>">
+						<?php } else {
+							echo esc_url(bloginfo('name'));
+						} ?>
 					</a>
