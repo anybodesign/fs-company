5ff4e8cafb1d050b96a638334592fab9b8d1119a
Thomas Villain <atelier@anybodesign.com>
2020-04-10 12:58:18 +0200

    Auto insert slide terms

--- inc/fs-cpt.php
+++ inc/fs-cpt.php
@@ -14,6 +14,7 @@ defined('ABSPATH') or die();
 
 function fs_rewrite_flush() {
     fs_custom_posts();
+    fs_insert_slide_term();
     flush_rewrite_rules();
 }
 add_action( 'after_switch_theme', 'fs_rewrite_flush' );
@@ -30,16 +31,6 @@ function fs_custom_posts() {
 		'singular_name'			=> _x( 'Service', 'Post Type Singular Name', 'fs-company' ),
 		'menu_name'				=> __( 'Services', 'fs-company' ),
 		'name_admin_bar'		=> __( 'Services', 'fs-company' ),
-/*
-		'parent_item_colon'		=> __( 'Parent service:', 'fs-company' ),
-		'all_items'				=> __( 'All services', 'fs-company' ),
-		'add_new_item'			=> __( 'Add New Service', 'fs-company' ),
-		'new_item'				=> __( 'New Service', 'fs-company' ),
-		'edit_item'				=> __( 'Edit Service', 'fs-company' ),
-		'update_item'			=> __( 'Update Service', 'fs-company' ),
-		'view_item'				=> __( 'View Service', 'fs-company' ),
-		'search_items'			=> __( 'Search Service', 'fs-company' ),
-*/
 	);
 	$rewrite = array(
 		//'slug'					=> 'service',
@@ -142,9 +133,9 @@ function fs_custom_taxonomies() {
 		'search_items'					=> __( 'Search Slide Category', 'fs-company' ),
 	);
 	$args = array(
-		'labels'				=> $labels,
+		'labels'					=> $labels,
 		'hierarchical'			=> true,
-		'public'				=> true,
+		'public'					=> true,
 		'show_ui'				=> true,
 		'show_admin_column'		=> true,
 		'show_in_nav_menus'		=> true,
@@ -152,40 +143,30 @@ function fs_custom_taxonomies() {
 		//'rewrite'				=> array( 'slug' => 'creations' ),		
 	);
 	register_taxonomy( 'slide-category', array( 'slide' ), $args );	
-
-	/*
-	$labels = array(
-		'name'							=> _x( 'Services Categories', 'Taxonomy General Name', 'fs-company' ),
-		'singular_name'					=> _x( 'Services Category', 'Taxonomy Singular Name', 'fs-company' ),
-		'menu_name'						=> __( 'Services Categories', 'fs-company' ),
-		
-		//'all_items'						=> __( 'All Services Categories', 'fs-company' ),
-		//'parent_item'					=> __( 'Parent Services Category', 'fs-company' ),
-		//'parent_item_colon'				=> __( 'Parent Services Category:', 'fs-company' ),
-		//'new_item_name'					=> __( 'New Services Category', 'fs-company' ),
-		//'add_new_item'					=> __( 'Add New Services Category', 'fs-company' ),
-		//'edit_item'						=> __( 'Edit Services Category', 'fs-company' ),
-		//'update_item'					=> __( 'Update Services Category', 'fs-company' ),
-		//'view_item'						=> __( 'View Services Category', 'fs-company' ),
-		//'popular_items'					=> __( 'Popular Services Category', 'fs-company' ),
-		//'search_items'					=> __( 'Search Services Category', 'fs-company' ),
-		
-	);
-	$args = array(
-		'labels'				=> $labels,
-		'hierarchical'			=> true,
-		'public'				=> true,
-		'show_ui'				=> true,
-		'show_admin_column'		=> true,
-		'show_in_nav_menus'		=> true,
-		'show_tagcloud'			=> false,
-		//'rewrite'				=> array( 'slug' => 'creations' ),		
-	);
-	register_taxonomy( 'service-category', array( 'service' ), $args );	
-	*/
 }
 add_action( 'init', 'fs_custom_taxonomies', 0 );
 
+
+// Add terms
+
+function fs_insert_slide_term() {
+    wp_insert_term(
+        __('Picture', 'fs-company'), // the term 
+        'slide-category', // the taxonomy
+        array(
+          'description' => __('Fullscreen background picture', 'fs-company'),
+          'slug'        => 'image'
+        )
+    );
+    wp_insert_term(
+        __('Video', 'fs-company'), 
+        'slide-category',
+        array(
+          'description' => __('Native mp4 background video', 'fs-company'),
+          'slug'        => 'video'
+        )
+    );
+}
 
 
 // Override CPT Names
@@ -212,69 +193,11 @@ if ( get_theme_mod('cpt_name_text') || get_theme_mod('cpt_plural_text') || get_t
 	        $args['rewrite']['slug'] = $cpt_slug;
 	        
 	        $args['label'] = __( $cpt_plural );
-	        //$args['description'] = 'Nos références';
-	        //$args['menu_icon'] = 'dashicons-building';
 	        
 	        $args['labels']['name'] = $cpt_plural;
 	        $args['labels']['singular_name'] = $cpt_singular;
 	        $args['labels']['menu_name'] = $cpt_plural;
 	        $args['labels']['name_admin_bar'] = $cpt_plural;
-/*
-	        $args['labels']['parent_item_colon'] = 'Référence parente :';
-	        $args['labels']['all_items'] = 'Toutes les références';
-	        $args['labels']['add_new_item'] = 'Ajouter une référence';
-	        $args['labels']['new_item'] = 'Nouvelle référence';
-	        $args['labels']['edit_item'] = 'Modifier la référence';
-	        $args['labels']['update_item'] = 'Mettre à jour la référence';
-	        $args['labels']['view_item'] = 'Voir la référence';
-	        $args['labels']['search_items'] = 'Chercher une référence';
-*/
-	    }
-	 
-	    return $args;
-	}
-
-}
-
-
-// Override Tax Names
-
-if ( get_theme_mod('tax_name_text') || get_theme_mod('tax_plural_text') || get_theme_mod('tax_slug') ) {
-
-	add_filter('register_taxonomy_args', 'any_fscompany_rewrite_tax', 10, 3);
-	function any_fscompany_rewrite_tax($args, $taxonomy){
- 
-		// var
-		
-		if(get_theme_mod('tax_name_text')) {
-			$tax_singular = get_theme_mod('tax_name_text'); 
-		}
-		if(get_theme_mod('tax_plural_text')) {
-			$tax_plural = get_theme_mod('tax_plural_text'); 
-		}
-		if(get_theme_mod('tax_slug')) {
-			$tax_slug = get_theme_mod('tax_slug'); 
-		}
-	 
-	    if ($taxonomy == 'service-category'){	
-	        
-	        $args['rewrite']['slug'] = $tax_slug;
-	        
-			$args['labels']['name'] = _x( $tax_plural, 'Taxonomy General Name');
-	        $args['labels']['singular_name'] = _x( $tax_singular, 'Taxonomy Singular Name');
-	        $args['labels']['menu_name'] = __( $tax_plural );
-			/*
-			$args['labels']['all_items'] = __( $tax_plural.': All' );
-	        $args['labels']['parent_item'] = __( 'Parent '.$tax_singular );
-	        $args['labels']['parent_item_colon'] = __( 'Parent: '. $tax_singular);
-	        $args['labels']['new_item_name'] = __( 'New '.$tax_singular );
-	        $args['labels']['add_new_item'] = __( 'Add '. $tax_singular );
-	        $args['labels']['edit_item'] = __( 'Edit '. $tax_singular);
-	        $args['labels']['update_item'] = __( 'Update '. $tax_singular);
-	        $args['labels']['view_item'] = __( 'View '. $tax_singular);
-	        $args['labels']['popular_items'] = __( 'Popular '. $tax_singular);
-	        $args['labels']['search_items'] = __( 'Search '. $tax_singular);
-	        */
 	    }
 	 
 	    return $args;
