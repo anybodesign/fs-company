5d463913e2af7d8a85f72fd332a12d5817ca86ce
Thomas Villain <atelier@anybodesign.com>
2020-04-20 18:26:01 +0200

    Header logo fix

--- template-parts/header-logo.php
+++ template-parts/header-logo.php
@@ -11,16 +11,14 @@
 ?>
 
 				
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php _e('Go to Home Page', 'fs-blog'); ?>">
-						<?php if ( get_theme_mod('site_logo_white') ) { // white version  ?>
-							<?php if ( is_front_page() || is_page_template('pagecustom-maintenance.php') ) { ?>
-							<img class="logo-white" src="<?php echo(get_theme_mod('site_logo_white', 'none')); ?>" alt="<?php echo esc_url(bloginfo('name')); ?>">
-							<?php } ?>
+					<?php if ( get_theme_mod('site_logo_white') ) { // white version  ?>
+						<?php if ( is_front_page() || is_page_template('pagecustom-maintenance.php') ) { ?>
+						<img class="logo-white" src="<?php echo(get_theme_mod('site_logo_white', 'none')); ?>" alt="<?php echo esc_url(bloginfo('name')); ?>">
 						<?php } ?>
-						
-						<?php if ( get_theme_mod('site_logo') ) { ?>
-							<img class="logo" src="<?php echo(get_theme_mod('site_logo', 'none')); ?>" alt="<?php echo esc_url(bloginfo('name')); ?>">
-						<?php } else {
-							echo esc_url(bloginfo('name'));
-						} ?>
-					</a>
+					<?php } ?>
+					
+					<?php if ( get_theme_mod('site_logo') ) { ?>
+						<img class="logo" src="<?php echo(get_theme_mod('site_logo', 'none')); ?>" alt="<?php echo esc_url(bloginfo('name')); ?>">
+					<?php } else {
+						echo esc_url(bloginfo('name'));
+					} ?>
